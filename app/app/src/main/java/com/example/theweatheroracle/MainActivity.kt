0e6f603af3822186d91d3fc7f3540105517bc268
AbdelrhmanAFayed <abdelrhmanfayed2002@gmail.com>
2025-05-23 00:01:24 +0300

    Added Splash Screen

--- app/app/src/main/java/com/example/theweatheroracle/MainActivity.kt
+++ app/app/src/main/java/com/example/theweatheroracle/MainActivity.kt
@@ -1,15 +1,38 @@
 package com.example.theweatheroracle
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.airbnb.lottie.LottieAnimationView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val lottieAnimationView = findViewById<LottieAnimationView>(R.id.lottie_splash)
+
+        lottieAnimationView.setAnimation(R.raw.splash)
+
+        lottieAnimationView.playAnimation()
+
+        lottieAnimationView.addAnimatorListener(object : android.animation.Animator.AnimatorListener {
+            override fun onAnimationStart(animation: android.animation.Animator) {}
+            override fun onAnimationEnd(animation: android.animation.Animator) {
+                startActivity(Intent(this@MainActivity, NavActivity::class.java))
+                finish()
+            }
+            override fun onAnimationCancel(animation: android.animation.Animator) {}
+            override fun onAnimationRepeat(animation: android.animation.Animator) {}
+        })
+
+
+
 
     }
 }
