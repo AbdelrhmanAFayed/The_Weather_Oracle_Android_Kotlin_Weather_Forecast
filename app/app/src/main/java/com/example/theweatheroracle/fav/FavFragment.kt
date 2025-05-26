1c62e7663f674ddb535febcaadbe87b66b168845
AbdelrhmanAFayed <abdelrhmanfayed2002@gmail.com>
2025-05-26 15:57:37 +0300

    Restructure model

--- app/app/src/main/java/com/example/theweatheroracle/fav/FavFragment.kt
+++ app/app/src/main/java/com/example/theweatheroracle/fav/FavFragment.kt
@@ -1,7 +1,6 @@
 package com.example.theweatheroracle.fav
 
 import android.annotation.SuppressLint
-import androidx.fragment.app.viewModels
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -14,9 +13,9 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.theweatheroracle.R
 import com.example.theweatheroracle.databinding.FragmentFavBinding
-import com.example.theweatheroracle.model.WeatherRepositoryImp
+import com.example.theweatheroracle.model.weather.WeatherRepositoryImp
 import com.example.theweatheroracle.model.api.WeatherRemoteDataSourceImpl
-import com.example.theweatheroracle.model.db.WeatherLocalDataSourceImpl
+import com.example.theweatheroracle.model.db.weather.WeatherLocalDataSourceImpl
 import com.example.theweatheroracle.model.settings.ISettingsManager
 import com.example.theweatheroracle.model.settings.SettingsManager
 
